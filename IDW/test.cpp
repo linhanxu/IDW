2638720ea817594a32967c55d74153b3ce74931e
Linghan <1326798376@qq.com>
2019-03-24 15:35:02 +0800

    反距离加权插值

---
+++ IDW/test.cpp
@@ -0,0 +1,14 @@
+#include "MyPoint.h"
+#include "IDW.h"
+
+int main()
+{
+	IDW mIDW = IDW();
+	vector<MyPoint> grid = mIDW.CreateGrid();
+	vector<MyPoint> discretePoints = mIDW.CreateDiscretePoints();
+
+	mIDW.Interpolate(grid, discretePoints, 2, 20);
+
+	getchar();
+	return 0;
+}
